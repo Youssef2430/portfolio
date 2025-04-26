0df61717fded4dc81766ec9ac95c2456127ec72e
Youssef30 <ychou031@uottawa.ca>
2025-04-26 19:19:36 -0400

    added posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,33 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { PostHogProvider } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Youssef Chouay | Software Engineer",
-  description: "Portfolio of Youssef Chouay, Software Engineer and AI Researcher",
-    generator: 'v0.dev'
-}
+  description:
+    "Portfolio of Youssef Chouay, Software Engineer and AI Researcher",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        <PostHogProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </PostHogProvider>
       </body>
     </html>
-  )
+  );
 }
